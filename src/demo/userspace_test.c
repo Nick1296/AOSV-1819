661a1ce7e3c6b8c263a0305cccc5c7084ccbe285
Nick1296 <mattianicolella@gmail.com>
2020-02-10 12:01:09 +0100

    perf(makefiles): wite makefiles for usespace program and shared lib
    With the provided makefiles the compilation of the shared lib and the userspace program can be done
    by issuing make from the src folder.
    
    Signed-off-by: Nick1296 <mattianicolella@gmail.com>

--- src/demo/userspace_test.c
+++ src/demo/userspace_test.c
@@ -1,10 +1,13 @@
+///we define the O_SESS flag which will enable sessions
 #define O_SESS 00000004
 #include <stdio.h>
-#include<fcntl.h>
-// simple test that tries to open a file in a directory enabling session semtics and checks the open result.
-int main(char* argv, int argc){
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char** argv){
 	int ret;
-	ret=open("/home/nick1296/Workspace/test.txt",O_SESS | O_CREAT);
+	ret=open("test.txt",O_SESS | O_CREAT);
 	printf("open result: %d\n",ret);
+	close(ret);
 	return 0;
 }
--- src/shared_lib/libsessionfs.h
+++ src/shared_lib/libsessionfs.h
@@ -3,32 +3,25 @@
  * to the SessionFS kernel module, because it uses the unix session semantics.
  */
 
-/** \file 
+/** \file
  * \brief Shared library header.
  * Header file for the shared library that wraps the `open` and `close` functions.
- */ 
+ */
 
 /// Enables RTLD_NEXT macro.
 #define _GNU_SOURCE
 // dlsym function
 #include <dlfcn.h>
-
 /** \brief Flag that enables the session semantic.
  *  Unused flag in `include/uapi/asm-generic/fcntl.h` that is repurposed to be used to enable the session semantic.
  */
 #define O_SESS 00000004
 
-///we use a typedef to alias the function pointer to the libc `open`.
-typedef int (*orig_open_type)(const char* pathname, int flags);
-
-/// we use a typedef to alis the function pointer to the libc `close`.
-typedef int (*orig_close_type)(int filedes);
-
 /**
  * \brief Wraps the open determining if it must call the libc `open` or the SessionFS module.
  * \param[in] pathname The pathname of the file to be opened, same usage an type of the libc `open`'s `pathname`.
  * \param[in] flags flags to determine the file status flag and the access modes, same as the libc `open`'s `oflag`, however a possible flag is the ::O_SESS flag which enables the session semantic.
- * \returns It will return a file descriptor if the operation is successful, both for the libc version and for the module return value. 
+ * \returns It will return a file descriptor if the operation is successful, both for the libc version and for the module return value.
  */
 int open(const char* pathname, int flags);
 
